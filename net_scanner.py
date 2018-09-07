179fe2e26fcb6bc7482e21d8f90de67378ca57b1
Hary Rodriguez <imu24r@gmail.com>
2018-09-07 15:32:11 -0600

    Update net_scanner.py

--- net_scanner.py
+++ net_scanner.py
@@ -3,11 +3,13 @@
 import scapy.all as scapy
 import argparse
 
+
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t" , "--target", dest="target", help= "Target IP / Range.")
     options = parser.parse_args()
     return options
+
 
 def scan(ip):
 
